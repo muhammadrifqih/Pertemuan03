bf27b2ab7415d2a60bf20cdce6ba0d609c6fdb6c
rifqi <rifqihidayatmuhammad@gmail.com>
2024-04-22 11:29:36 +0800

    first commit

---
+++ InputOutput.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class InputOutput {
+    public static void main(String[] args) {
+        int nilai = 0;
+        double rata = 0;
+        String nama = "rifqi"
+        Scanner scanner = new Scanner(System.in);
+    }
+}
