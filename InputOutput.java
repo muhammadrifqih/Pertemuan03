cdd876af0c2e4bc41f387562a92d5b57b6a6fb1d
rifqi <rifqihidayatmuhammad@gmail.com>
2024-04-22 11:54:25 +0800

    me edit code semuaan

--- InputOutput.java
+++ InputOutput.java
@@ -2,14 +2,23 @@ import java.util.Scanner;
 
 public class InputOutput {
     public static void main(String[] args) {
-        int nilai = 0;
-        double rata = 0;
-        String nama = "rifqi";
         Scanner scanner = new Scanner(System.in);
+        String nama;
+        int jumlahSks;
+        double IPK;
 
-        System.out.println("Nama :" + nama);
-        System.out.println("Input Nama Baru: ");
+        System.out.println("Masukkan Nama Anda : ");
         nama = scanner.nextLine();
-        System.out.println("Nama Baru : " + nama);
+        System.out.println("Hai, " + nama);
+
+        System.out.println("Masukan Jumlah SKS yang sudah di tempuh :");
+        jumlahSks = scanner.nextInt();
+
+        System.out.println("Masukan IPK Semester");
+        IPK = scanner.nextDouble();
+
+        double Jumlah = IPK * jumlahSks;
+        System.out.println("Total Nilai = " + Jumlah);
+
     }
 }
