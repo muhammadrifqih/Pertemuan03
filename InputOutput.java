25cbffe5bdbcd35ae8155e41634002deb9ff18a3
rifqi <rifqihidayatmuhammad@gmail.com>
2024-04-22 11:40:21 +0800

    git pertama

--- InputOutput.java
+++ InputOutput.java
@@ -4,7 +4,12 @@ public class InputOutput {
     public static void main(String[] args) {
         int nilai = 0;
         double rata = 0;
-        String nama = "rifqi"
+        String nama = "rifqi";
         Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Nama :" + nama);
+        System.out.println("Input Nama Baru: ");
+        nama = scanner.nextLine();
+        System.out.println("Nama Baru: " + nama);
     }
 }
